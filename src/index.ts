7842a83e6bd3ed11148f18d98bf8de15b23581dd
Jani Sillanpää <janzku1993@hotmail.com>
2023-12-22 06:37:05 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -7,96 +7,88 @@ import ENV from "./env";
  */
 const unknownHands: string[] = [];
 
-const games = {
-  sevenCardStud: "7 Card Stud",
-  sevenCardStudHiLo: "7 Card Stud Hi/Lo",
-  razz: "Razz",
-  tripleDrawLowball: "Triple Draw 2-7 Lowball",
-  holdEmNolimit: "Hold'em No Limit",
-  holdEmLimit: "Hold'em Limit",
-  omahaHiLoLimit: "Omaha Hi/Lo Limit",
-  omahaPotLimit: "Omaha Pot Limit",
-  omahaHiLoPotLimit: "Omaha Hi/Lo Pot Limit",
-  UNKNOWN: "UNKNOWN",
-} as const;
+type PokerGame =
+  | "7 Card Stud Hi/Lo"
+  | "7 Card Stud"
+  | "Hold'em Limit"
+  | "Hold'em No Limit"
+  | "Omaha Hi/Lo Limit"
+  | "Omaha Hi/Lo Pot Limit"
+  | "Triple Draw 2-7 Lowball"
+  | "Omaha Pot Limit"
+  | "Razz";
 
-const playedHands: Record<string, number> = {
-  "7 Card Stud": 0,
+const playedHands: Record<PokerGame | "UNKNOWN", number> = {
   "7 Card Stud Hi/Lo": 0,
-  Razz: 0,
-  "Triple Draw 2-7 Lowball": 0,
-  "Hold'em No Limit": 0,
+  "7 Card Stud": 0,
   "Hold'em Limit": 0,
+  "Hold'em No Limit": 0,
   "Omaha Hi/Lo Limit": 0,
-  "Omaha Pot Limit": 0,
   "Omaha Hi/Lo Pot Limit": 0,
+  "Triple Draw 2-7 Lowball": 0,
+  "Omaha Pot Limit": 0,
+  Razz: 0,
   UNKNOWN: 0,
 };
 
-const countHandsInFile = (filePath: string) => {
+const readAllHandHistoryFiles = (folderPath: string) =>
+  fs
+    .readdirSync(folderPath)
+    .filter((filename) => filename.endsWith(".txt"))
+    .forEach((filename) =>
+      getHandLinesFromFiles(path.join(folderPath, filename)),
+    );
+
+const getHandLinesFromFiles = (filePath: string) => {
   const content = fs.readFileSync(filePath, "utf8");
   const handPattern = /PokerStars Hand #\d+/g;
 
   const lines = content.split("\n");
   const matchingLines = lines.filter((line) => handPattern.test(line));
-  addPlayedHandToGameGroup(matchingLines);
+  calculatePlayedHands(matchingLines);
 };
 
-const countHandsInFolder = (folderPath: string) =>
-  fs
-    .readdirSync(folderPath)
-    .filter((filename) => filename.endsWith(".txt"))
-    .forEach((filename) => countHandsInFile(path.join(folderPath, filename)));
+const calculatePlayedHands = (textLines: string[]) =>
+  textLines.forEach((handTextLine) => {
+    const pokerGames: PokerGame[] = [
+      "7 Card Stud Hi/Lo",
+      "7 Card Stud",
+      "Hold'em Limit",
+      "Hold'em No Limit",
+      "Omaha Hi/Lo Limit",
+      "Omaha Hi/Lo Pot Limit",
+      "Triple Draw 2-7 Lowball",
+      "Omaha Pot Limit",
+      "Razz",
+    ];
 
-const addPlayedHandToGameGroup = (hands: string[]) => {
-  hands.forEach((hand) => {
-    switch (true) {
-      case hand.includes(games.sevenCardStudHiLo):
-        playedHands["7 Card Stud Hi/Lo"]++;
-        break;
-      case hand.includes(games.sevenCardStud):
-        playedHands["7 Card Stud"]++;
-        break;
-      case hand.includes(games.holdEmLimit):
-        playedHands["Hold'em Limit"]++;
-        break;
-      case hand.includes(games.holdEmNolimit):
-        playedHands["Hold'em No Limit"]++;
-        break;
-      case hand.includes(games.omahaHiLoLimit):
-        playedHands["Omaha Hi/Lo Limit"]++;
-        break;
-      case hand.includes(games.omahaHiLoPotLimit):
-        playedHands["Omaha Hi/Lo Pot Limit"]++;
-        break;
-      case hand.includes(games.tripleDrawLowball):
-        playedHands["Triple Draw 2-7 Lowball"]++;
-        break;
-      case hand.includes(games.omahaPotLimit):
-        playedHands["Omaha Pot Limit"]++;
-        break;
-      case hand.includes(games.razz):
-        playedHands["Razz"]++;
-        break;
-      default:
-        playedHands.UNKNOWN++;
-        unknownHands.push(hand);
+    const matchedGame = pokerGames.find((game) => handTextLine.includes(game));
+
+    if (!matchedGame) {
+      playedHands.UNKNOWN++;
+      unknownHands.push(handTextLine);
+      return;
     }
-  });
-};
 
-const logHandsByGame = () => {
+    playedHands[matchedGame]++;
+  });
+
+const logPlayedHands = () => {
+  // Sort bu played hands descending
   const sortedGames = Object.keys(playedHands).sort(
-    (a, b) => playedHands[b] - playedHands[a],
+    (a, b) =>
+      playedHands[b as PokerGame | "UNKNOWN"] -
+      playedHands[a as PokerGame | "UNKNOWN"],
   );
 
+  // Get max string length so played count can be aligned vertically
   const maxGameNameLength = sortedGames.reduce(
     (max, game) => Math.max(max, game.length),
     0,
   );
 
   sortedGames.forEach((game) => {
-    const gameCount = playedHands[game];
+    const gameCount = playedHands[game as PokerGame | "UNKNOWN"];
 
     if (gameCount === 0) return;
 
@@ -116,11 +108,11 @@ const logHandsByGame = () => {
 };
 
 /**
- * Set absolute path to PokerStar folder where is hand history stored
+ * Set absolute path to PokerStar folder where hand history is stored
  */
-countHandsInFolder(ENV.historyFolderPath);
+readAllHandHistoryFiles(ENV.historyFolderPath);
 
-logHandsByGame();
+logPlayedHands();
 
 function printErrorIfUnknownGamesExists() {
   console.log(
