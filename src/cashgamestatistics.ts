85fcdbc4d3e1d2503a0c08c347858d982c58bfe6
Jani Sillanpää <janzku1993@hotmail.com>
2023-12-29 06:32:40 +0200

    cashgamestatistics not implemented yet

--- src/cashgamestatistics.ts
+++ src/cashgamestatistics.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import ENV from './utils/env';
+import { PokerGame, createPlayedHandsObject } from './types';
 
 const SUMMARY_HEADER = '*** SUMMARY ***';
 const POKERSTARTS_HEADER = 'PokerStars Hand #';
@@ -10,62 +11,14 @@ type WinningTerm = 'collected' | 'won';
 type ActionTerm = 'bets' | 'calls' | 'raises' | 'posts';
 type Term = WinningTerm | LosingTerm | ActionTerm;
 
-type SummaryHeader = typeof SUMMARY_HEADER;
-type PokerStarsHeader = typeof POKERSTARTS_HEADER;
+// Value is number so its valid for calculation cash sums
+const _wonGames = createPlayedHandsObject();
+const _lostGames = createPlayedHandsObject();
 
-type PokerHeader = SummaryHeader | PokerStarsHeader;
+let rake = 0;
+let playedHands = 0;
 
-type PokerGame =
-  | '7 Card Stud Hi/Lo'
-  | '7 Card Stud'
-  | "Hold'em Limit"
-  | "Hold'em No Limit"
-  | 'Omaha Hi/Lo Limit'
-  | 'Omaha Hi/Lo Pot Limit'
-  | 'Triple Draw 2-7 Lowball'
-  | 'Omaha Pot Limit'
-  | 'Razz'
-  | 'UNKNOWN';
-
-const pokerGames: PokerGame[] = [
-  '7 Card Stud Hi/Lo',
-  '7 Card Stud',
-  "Hold'em Limit",
-  "Hold'em No Limit",
-  'Omaha Hi/Lo Limit',
-  'Omaha Hi/Lo Pot Limit',
-  'Triple Draw 2-7 Lowball',
-  'Omaha Pot Limit',
-  'Razz'
-];
-
-const _earnGames: Record<PokerGame, number> = {
-  '7 Card Stud Hi/Lo': 0,
-  '7 Card Stud': 0,
-  "Hold'em Limit": 0,
-  "Hold'em No Limit": 0,
-  'Omaha Hi/Lo Limit': 0,
-  'Omaha Hi/Lo Pot Limit': 0,
-  'Triple Draw 2-7 Lowball': 0,
-  'Omaha Pot Limit': 0,
-  Razz: 0,
-  UNKNOWN: 0
-};
-
-const _wonGames: Record<PokerGame, number> = {
-  '7 Card Stud Hi/Lo': 0,
-  '7 Card Stud': 0,
-  "Hold'em Limit": 0,
-  "Hold'em No Limit": 0,
-  'Omaha Hi/Lo Limit': 0,
-  'Omaha Hi/Lo Pot Limit': 0,
-  'Triple Draw 2-7 Lowball': 0,
-  'Omaha Pot Limit': 0,
-  Razz: 0,
-  UNKNOWN: 0
-};
-
-const txtContentByGame: Record<PokerGame, string[][]> = {
+const txtContentByGame: Record<PokerGame | 'UNKNOWN', string[][]> = {
   '7 Card Stud Hi/Lo': [],
   '7 Card Stud': [],
   "Hold'em Limit": [],
@@ -78,89 +31,15 @@ const txtContentByGame: Record<PokerGame, string[][]> = {
   UNKNOWN: []
 };
 
-let rake = 0;
-let playedHands = 0;
-
 const readAllCashGameEarnings = (folderPath: string) => {
-  fs.readdirSync(folderPath)
-    .filter((filename) => filename.endsWith('.txt'))
-    .forEach((filename) => parseFileText(path.join(folderPath, filename)));
-};
+  const filepaths = fs
+    .readdirSync(folderPath)
+    .filter((filename) => filename.endsWith('.txt'));
 
-const parseFileText = (filePath: string) => {
-  const content = fs.readFileSync(filePath, 'utf8');
-  const linebreak = '\r\n';
-
-  const lines = content.split(linebreak);
-
-  const inputLines = lines.filter(
-    (line) => isLineHeader(line) || isLineKeywordAndPlayer(line)
-  );
-
-  const headerAndSummary: Record<string, string[]> = {};
-  let currentHand: string[] = [];
-  let currentHeader = '';
-
-  for (const line of inputLines) {
-    if (line.startsWith('PokerStars Hand #')) {
-      if (currentHand.length > 0) {
-        headerAndSummary[currentHeader.trim()] = [...currentHand];
-        currentHand = [];
-      }
-      // Set the current header
-      currentHeader = line;
-    } else {
-      // Include all lines for the current hand
-      currentHand.push(line);
-    }
-  }
-
-  if (currentHand.length > 0) {
-    const test = currentHeader.includes(POKERSTARTS_HEADER);
-    if (!test) console.log(currentHeader);
-    headerAndSummary[currentHeader.trim()] = [...currentHand];
-  }
-
-  calculateEarningsFromParsedText(headerAndSummary);
-};
-
-const calculateEarningsFromParsedText = (records: Record<string, string[]>) => {
-  Object.entries(records).forEach(([key, value]) => {
-    const matchedGame = pokerGames.find((game) => key.includes(game));
-
-    if (!matchedGame) {
-      /*      console.log('unknown', value); */
-      _wonGames.UNKNOWN++;
-      return;
-    }
-
-    const isWin = (lines: string[]): number | null => {
-      const collectedLines = lines.filter((line) => line.includes('collected'));
-
-      if (!collectedLines.length) return null;
-
-      return collectedLines
-        .filter((line) => line.includes(`${ENV.PLAYER_NAME} collected`))
-        .map((line) => Number(line.split(' ')[2]))
-        .reduce((acc, curr) => acc + curr, 0);
-    };
-
-    const winSum = isWin(value);
-    if (!winSum) return;
-
-    _earnGames[matchedGame] += winSum;
-    _wonGames[matchedGame]++;
-  });
-
-  logData();
+  throw new Error('Not implemented yet');
 };
 
 readAllCashGameEarnings(ENV.HAND_HISTORY_FOLDER_PATH);
-
-function logData() {
-  /*   console.log('_wonGames', _wonGames);
-  console.log('_earnGames', _earnGames); */
-}
 
 // Helper functions
 function isLineKeywordAndPlayer(line: string) {
@@ -178,11 +57,5 @@ function isLineKeywordAndPlayer(line: string) {
   return (
     keywords.some((word) => line.includes(word)) &&
     line.includes(ENV.PLAYER_NAME)
-  );
-}
-
-function isLineHeader(line: string): boolean {
-  return ['*** SUMMARY ***', 'PokerStars Hand #'].some((word) =>
-    line.includes(word)
   );
 }
